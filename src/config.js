9265d9740e22e220188c469d9a5508d9cd4ddd3e
AangelVT <angelovelaz32@gmail.com>
2024-11-12 16:31:06 -0600

    removed certificate related code in config

--- src/config.js
+++ src/config.js
@@ -3,8 +3,4 @@ import fs from 'fs';
 export default {
     TOKENS_EXP: 28800000,
     ALLOWED_ORIGINS: ['http://licencias.imduyv.gob.mx'],
-    HTTPS_OPTIONS: {
-        key: fs.readFileSync(process.env.CERT_KEY),
-        cert: fs.readFileSync(process.env.CERT_CRT)
-    }
 }
