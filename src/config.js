6ffe88e3b54e403c83198feb9cb656728b00ebc5
AangelVT <angelovelaz32@gmail.com>
2024-11-12 16:39:52 -0600

    defined routes and controllers for administration data

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-import fs from 'fs';
-
 export default {
     TOKENS_EXP: 28800000,
     ALLOWED_ORIGINS: ['http://licencias.imduyv.gob.mx'],
--- src/controllers/administration.controller.js
+++ src/controllers/administration.controller.js
@@ -1,21 +1,31 @@
 import { AdministrationDetails } from "../models/Administration.models.js";
 
 export const getPeriods = async (req, res) => {
-
+    res.send().json({
+        msg: "getting all periods"
+    });
 }
 
 export const getPeriod = async (req, res) => {
-
+    res.send().json({
+        msg: "getting period"
+    });
 }
 
 export const createPeriod = async (req, res) => {
-
+    res.send().json({
+        msg: "creating period"
+    });
 }
 
 export const updatePeriod = async (req, res) => {
-
+    res.send().json({
+        msg: "updating period"
+    });
 }
 
 export const deletePeriod = async (req, res) => {
-
+    res.send().json({
+        msg: "deleting period"
+    });
 }
