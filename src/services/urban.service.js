c27015ceacf61cd5a5c765628405efedcd559813
AangelVT <angelovelaz32@gmail.com>
2024-12-20 09:30:54 -0600

    added urban refactorization files

---
+++ src/services/urban.service.js
@@ -0,0 +1,24 @@
+
+export async function requestAllUrbanLicenses() {
+}
+
+export async function requestUrbanLicenseByInvoice(type, invoice, year) {
+}
+
+export async function requestUrbanLicenseByType(type, year) {
+}
+
+export async function requestUrbanLicenseByParameter(parameter, value) {
+}
+
+export async function requestUrbanLicenseCreate(body, file, requestor) {
+}
+
+export async function requestUrbanLicenseUpdate(id, licenseData, file, requestor) {
+}
+
+export async function requestUrbanLicenseDelete(id) {
+}
+
+export async function requestPDFDefinition(type, invoice, year) {
+}
