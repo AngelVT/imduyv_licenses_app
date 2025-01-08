e7c96a348f396f975386b493824eb3272a085326
AangelVT <angelovelaz32@gmail.com>
2025-01-08 15:10:08 -0600

    fixed new location of not found page

--- src/app.js
+++ src/app.js
@@ -69,7 +69,7 @@ app.use('/app', appRoutes);
 app.use('/test', testRoutes);
 
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notfound.html'));
+    res.sendFile(path.join(__dirname, 'resources','public', 'notfound.html'));
 });
 
 export default app;
